725e8dd3d617abbab3445b8b7f8721ec04590bda
FlorenciaRusso9606 <russoflorencia96@gmail.com>
2025-08-05 16:59:28 -0300

    Cambiando estilos del navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const linkClass = `
 `;
 
 const Navbar = () => (
-  <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur bg-[var(--bg-color)] shadow px-6 py-4 flex justify-between items-center">
+  <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur bg-[var(--navbar-bg)] shadow px-6 py-4 flex justify-between items-center">
 
     <a
       href="#home"
